f8e8dd30d8dc404f0c1d7b91256607c19e7583a3
t3ls <t3ls.bupt@gmail.com>
2022-05-25 17:18:25 +0800

    test

--- common/libc.sh
+++ common/libc.sh
@@ -167,9 +167,9 @@ get_all_debian() {
   local info=$1
   local url=$2
   local pkgname=$3
-  for f in `wget $url/ -O - 2>/dev/null | grep -Eoh "$pkgname"'(-i386|-amd64|-x32)?_[^"]*(amd64|i386)\.deb' |grep -v "</a>" | uniq`; do
-    get_debian "$url/$f" "$info" "$pkgname"
-  done
+  # for f in `wget $url/ -O - 2>/dev/null | grep -Eoh "$pkgname"'(-i386|-amd64|-x32)?_[^"]*(amd64|i386)\.deb' |grep -v "</a>" | uniq`; do
+  wget $url/ -O - 2>/dev/null | grep -Eoh "$pkgname"'(-i386|-amd64|-x32)?_[^"]*(amd64|i386)\.deb' |grep -v "</a>" | uniq | parallel get_debian "$url/{}" "$info" "$pkgname"
+  # done
   return 0
 }
 
