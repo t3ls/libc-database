8853835803b0cd5c786e03802ded24db2fee5d51
t3ls <t3ls.bupt@gmail.com>
2022-05-29 19:48:36 +0800

    t

--- common/libc.sh
+++ common/libc.sh
@@ -475,7 +475,7 @@ get_all_launchpad_i386() {
     local url=""
     wget "$apiurl" -O - 2>/dev/null | jq '[ .entries[] | .build_link + "/+files/" + .binary_package_name + "_" + .source_package_version + "_" + (.distro_arch_series_link | split("/") | .[-1]) + ".deb" | ltrimstr("https://api.launchpad.net/1.0/") | "https://launchpad.net/" + . ] | unique | .[]' |\
       parallel echo {} | grep -Eo '[^"]+' | grep -q 'i.86\.deb' | \
-      parallel -j 20 bash -c \"echo "get_debian {1} "$info-$series" "$pkgname" "$static""\" :::: -
+      echo \"get_debian - "$info-$series" "$pkgname" "$static"\"
   done
 }
 
